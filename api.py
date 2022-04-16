b4b5c3e687d3c5f64d81621d0f1a57562876a875
AbdullrahmanHMD <manofsteel272@yahoo.com>
2022-04-17 00:17:11 +0300

    Add arguments for delete and get

--- api.py
+++ api.py
@@ -10,12 +10,20 @@ user_add_args = reqparse.RequestParser()
 COLUMNS = ['user_id', 'age', 'email',
             'first_name', 'last_name', 'password']
 
+# Adding the query arguments:
 user_add_args.add_argument("user_id", type=int, help="The ID of the user", required=True)
 user_add_args.add_argument("age", type=int, help="The age of the user", required=True)
 user_add_args.add_argument("first_name", type=str, help="The first name of the user", required=True)
 user_add_args.add_argument("last_name", type=str, help="The last name of the user", required=True)
 user_add_args.add_argument("email", type=str, help="The email of the user", required=True)
 user_add_args.add_argument("password", type=str, help="The password of the user", required=True)
+
+user_delete_args = reqparse.RequestParser()
+user_delete_args.add_argument("user_id", type=int, help="The ID of the user", required=True)
+
+user_get_args = reqparse.RequestParser()
+user_get_args.add_argument("user_id", type=int, help="The ID of the user", required=True)
+# ------------------------------------------------------------------------------------------------------
 
 db_mgr = DbManager()
 
@@ -33,6 +41,7 @@ def abort_on_user_exists(user_id):
 class User(Resource):
     def get(self, user_id):
         # abort_on_user_does_not_exist(user_id)
+        args = user_get_args.parse_args()
         return db_mgr.get_user(user_id=user_id)
     
     def post(self, user_id):
@@ -53,10 +62,12 @@ class User(Resource):
     def delete(self, user_id):
         
         # abort_on_user_does_not_exist(user_id)
+        
+        args = user_delete_args.parse_args()
         return db_mgr.delete_user(user_id=user_id)
 
 
-# api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(User, "/user/<int:user_id>")
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
