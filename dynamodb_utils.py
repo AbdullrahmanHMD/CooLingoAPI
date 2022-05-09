ac55e4a01e3a16a0417f0758516627621d0401ce
AbdullrahmanHMD <manofsteel272@yahoo.com>
2022-05-10 01:28:29 +0300

    Handle the error where when a delete request will make the word list empty

--- dynamodb_utils.py
+++ dynamodb_utils.py
@@ -168,8 +168,13 @@ class DbManager():
         try:
             user = self.USERS_TABLE.get_item(Key=key)['Item']
             new_word_list = user['words']
-            for word in words:
-                new_word_list.remove(word)
+            
+            if len(user['words']) <= len(words):
+                new_word_list = []    
+                
+            else:
+                for word in words:
+                    new_word_list.remove(word)
             
             user['words'] = new_word_list
             response = self.USERS_TABLE.put_item(Item=user)
