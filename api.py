9a49d2ee74283aff581261b41e913b9a8124a6b1
AbdullrahmanHMD <manofsteel272@yahoo.com>
2022-05-15 04:01:20 +0300

    Remove resouces folder and define AverageLanguageErrors resource in api.py

--- api.py
+++ api.py
@@ -290,7 +290,34 @@ class LanguageErrors(Resource):
         
         return json_response
     
+# --- User average language errors arguments ---------------------------------------------------------------------------------------
+    
+avg_lang_errors_args = reqparse.RequestParser()
+avg_lang_errors_args.add_argument("email", type=str, help="The email of the user.", required=True)
+avg_lang_errors_args.add_argument("session_errors", type=int, help="The last session time spent on the app by the user.", required=True)
 
+# ------------------------------------------------------------------------------------------------------------------
+
+class AverageLanguageErrors(Resource):
+    def post(self):
+        args = avg_lang_errors_args.parse_args()
+        email = args['email']
+        session_errors = args['session_errors']
+        
+        response, status = db_mgr.add_avg_lang_error(email, session_errors)
+        
+        json_response = jsonify(response=response, status=status)
+        
+        return json_response
+        
+    def get(self):
+        email = request.args.get('email')
+        
+        response, status = db_mgr.get_avg_lang_error(email)
+        
+        json_response = jsonify(response=response, status=status)
+        
+        return json_response
 
 # --- User login number arguments ---------------------------------------------------------------------------------------
     
@@ -316,9 +343,6 @@ class LoginNumber(Resource):
         
         return json_response
 
-from resources.avg_lang_errors import AverageLanguageErrors
-
-avg = AverageLanguageErrors(db_mgr=db_mgr)
 
 api.add_resource(Authentication, "/auth")    
 api.add_resource(User, "/users")
@@ -329,7 +353,7 @@ api.add_resource(Questions, "/questions")
 api.add_resource(TotalTimeSpent, "/total_time")
 api.add_resource(AverageTimeSpent, "/avg_time")
 api.add_resource(LanguageErrors, "/lang_errors")
-api.add_resource(avg, "/avg_lang_errors")
+api.add_resource(AverageLanguageErrors, "/avg_lang_errors")
 api.add_resource(LoginNumber, "/login_num")
 
 if __name__ == "__main__":
